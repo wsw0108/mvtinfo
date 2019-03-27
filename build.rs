be89a7aac014f4e1926b1f08a7b38cab6b24ab64
wsw0108 <wsw0108@gmail.com>
2019-03-27 20:54:42 +0800

    first version

--- build.rs
+++ build.rs
@@ -2,10 +2,12 @@ use protoc_rust::Customize;
 
 fn main() {
     protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/proto",
-        input: &["proto/vector_tile.proto"],
-        includes: &["proto"],
+        out_dir: "src",
+        input: &["vector_tile.proto"],
+        includes: &[],
         customize: Customize {
+            carllerche_bytes_for_bytes: Some(true),
+            carllerche_bytes_for_string: Some(true),
             ..Default::default()
         },
     }).expect("protoc");
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use std::path::Path;
 use clap::{App, Arg};
 use reqwest::Url;
 
-fn main() -> Result<(), std::error::Error> {
+use vector_tile::Tile;
+
+mod vector_tile;
+
+fn main() {
     let matches = App::new("mvtinfo")
         .version("0.1.0")
         .about("Display info about mapbox vector tile")
@@ -17,18 +21,26 @@ fn main() -> Result<(), std::error::Error> {
                 .help("target to parse"),
         )
         .get_matches();
-    let target = matches.value_of("TARGET")?;
-    let mut bytes: Vec<u8>;
+    let target = matches.value_of("TARGET").unwrap();
+    let mut bytes: Vec<u8> = Vec::new();
     if target.starts_with("http://") || target.starts_with("https://") {
-        let url = Url::parse(target)?;
-        let client = reqwest::ClientBuilder::new().build()?;
-        let mut response = client.get(url).send()?;
-        response.read_to_end(&mut bytes)
+        let url = Url::parse(target).unwrap();
+        let client = reqwest::ClientBuilder::new().build().unwrap();
+        let mut response = client.get(url).send().unwrap();
+        response.read_to_end(&mut bytes).unwrap();
     } else {
         let path = Path::new(target);
-        let file = File::open(path)?;
+        let file = File::open(path).unwrap();
         let mut reader = BufReader::new(file);
-        reader.read_to_end(&mut bytes)
+        reader.read_to_end(&mut bytes).unwrap();
     }
-    Ok(())
+    let tile: Tile = protobuf::parse_from_bytes(bytes.as_ref()).unwrap();
+    for layer in tile.get_layers() {
+        println!("Layer:");
+        println!("\tName: {}", layer.get_name());
+        println!("\tVersion: {}", layer.get_version());
+        println!("\tExtent: {}", layer.get_extent());
+        println!("\tFields: {:?}", layer.get_keys());
+        println!("\tCount: {}", layer.get_features().len());
+    }
 }
